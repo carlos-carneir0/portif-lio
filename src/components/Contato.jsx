9c8a52c5e1ea35e9604dbf204b37537e2fa2477f
Carlos Eduardo Acciarito Carneiro <carloscaducarneiro@gmail.com>
2025-04-27 16:42:26 -0300

    27/04/25
    Alteração no design na área de "contato"

--- src/components/Contato.jsx
+++ src/components/Contato.jsx
@@ -5,14 +5,29 @@ function Contato() {
   return (
     <div className="contato-container">
       <h1>Contato</h1>
-      <p>Entre em contato comigo pelo e-mail: <a href="mailto:seuemail@exemplo.com">seuemail@exemplo.com</a></p>
-      <p>Ou preencha o formulário abaixo:</p>
-      <form>
-        <input type="text" placeholder="Seu nome" required />
-        <input type="email" placeholder="Seu e-mail" required />
-        <textarea placeholder="Sua mensagem" required />
-        <button type="submit">Enviar</button>
-      </form>
+      <p style={{fontSize: '1.2rem'}}>Entre em contato comigo pelo e-mail:<br/>
+        <a className="contato-email" href="mailto:carloseacarneiro@gmail.com">carloseacarneiro@gmail.com</a>
+      </p>
+      <div className="contato-social-icons">
+        <a
+          className="contato-icon-btn linkedin"
+          href="https://www.linkedin.com/in/seu-usuario" 
+          target="_blank"
+          rel="noopener noreferrer"
+          title="LinkedIn"
+        >
+          <img src={require('../icons/linkedin.svg').default} alt="LinkedIn" className="contato-icon" />
+        </a>
+        <a
+          className="contato-icon-btn github"
+          href="https://github.com/carlos-carneir0" 
+          target="_blank"
+          rel="noopener noreferrer"
+          title="GitHub"
+        >
+          <img src={require('../icons/github.svg').default} alt="GitHub" className="contato-icon" />
+        </a>
+      </div>
     </div>
   );
 }
